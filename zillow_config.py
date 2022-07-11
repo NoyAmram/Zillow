f311d20a0188cfb2ba46593b42433a193aae1569
Noy Amram <noy.amram@gmail.com>
2022-07-11 19:23:35 +0300

    first draft of data mining project_firstmilestone; upload main.py, config.py and requirements.txt

---
+++ zillow_config.py
@@ -0,0 +1,8 @@
+URL = "https://www.zillow.com/homes/San-Francisco,-CA_rb/"
+
+HEADER = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+
+PATTERN = "https://www.zillow.com"
---
+++ zillow_main.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+import requests
+import zillow_config as cfg
+import json
+
+
+def web_parse(url):
+    """ Receives  URL extracts ###to complete ,using BeautifulSoup library"""
+    response = requests.get(url, headers=cfg.HEADER)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup
+
+
+def data_scraping(web_soup):
+    """ Receives soup object, extract the content using json and returns data for each house as dictionary """
+    data = json.loads(
+        web_soup.select_one("script[data-zrr-shared-data-key]")
+        .contents[0]
+        .strip("!<>-")
+    )
+    all_data = data['cat1']['searchResults']['listResults']
+    return all_data
+
+
+def select_data(web_data):
+    """ Receives site content containing all data for each house as dictionary.
+     Extracts from the data: price, address, area, room number and link for each house """
+    for i in range(len(web_data)):
+        address = web_data[i]['address']
+        area = web_data[i]['area']
+        bed_rooms = web_data[i]['beds']
+        bath_rooms = web_data[i]['baths']
+
+        # some items have the 'price' key nested inside units key, while others have simply inside data key
+        try:
+            price = web_data[i]['units'][0]['price']
+        except KeyError:
+            price = web_data[i]['price']
+
+        link = web_data[i]['detailUrl']
+        # Some links do not contain the starting website url, inserting pattern of https://www.zillow.com
+        if 'http' not in link:
+            link_to_buy = cfg.PATTERN + link
+        else:
+            link_to_buy = link
+
+        print(f"{address} | {price} | {link_to_buy} | {area} sqft | {bed_rooms} bed rooms | {bath_rooms} bath rooms")
+
+
+def main():
+    """ Starting function of the program, calls above functions """
+    data_soup = web_parse(cfg.URL)
+    data = data_scraping(data_soup)
+    select_data(data)
+
+
+if __name__ == '__main__':
+    main()
