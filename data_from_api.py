f15a2bd12be6d268c6e0f47b261baff4ea8549a2
Noy Amram <noy.amram@gmail.com>
2022-08-15 17:01:36 +0300

    change unit_data_scraping file to be data_from_api and draft functions for requests from api. Update zillow_config with comments and api key, url and parameters

---
+++ data_from_api.py
@@ -0,0 +1,38 @@
+import requests
+import zillow_config as cfg
+import json
+import pandas as pd
+
+
+def get_data_from_api(api_url, parameters):
+    """ Receives API url and relevant parameters for query (including API key).
+     Using request, send request and extract content.
+      Returns data in json format"""
+    response = requests.get(api_url, parameters)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        pass
+        # to implement logger info
+
+
+def api_data_to_frame(api_data):
+    """ Receives content from API, according to query.
+         Extracts the data to create data frame relevant to table """
+    frame = pd.read_json(api_data, orient='index')
+    return frame
+
+
+def main():
+    """ Starting function of the program, calls above functions """
+    # adding information for schools table from API
+    schools_data = get_data_from_api(cfg.SCHOOLS_URL, cfg.SCHOOLS_PARAMETERS)
+    schools_df = api_data_to_frame(schools_data)
+
+    # adding information for area table from API
+    # to complete
+
+
+if __name__ == '__main__':
+    main()
--- zillow_config.py
+++ zillow_config.py
@@ -1,5 +1,6 @@
 from db_config import db_password, db_root
 
+# for main zillow_site_scraping
 HEADER = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
@@ -11,5 +12,12 @@ CITY = 'San-Diego'
 STATE = 'CA'
 BASE_URL = 'https://www.zillow.com/homes/'
 
+# for connection to MySQL
 SQL_PASS = db_password
 SQL_ROOT = db_root
+
+# for requests from APIs
+SCHOOLS_URL = "https://collegescorecard.ed.gov/"
+SCHOOLS_API_KEY = "to complete with hidden key"
+SCHOOLS_PARAMETERS = {
+    "appid": SCHOOLS_API_KEY}
