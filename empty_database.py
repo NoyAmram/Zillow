03a7b3cda6b1fc50c995608deb6ddccbc47aa1d7
Jia <yingjiaengineer@gmail.com>
2022-08-16 08:56:39 +0300

    empty database sctipt updated

--- empty_database.py
+++ empty_database.py
@@ -28,81 +28,82 @@ def create_tables(cursor):
     """Creates tables inside database zillow. """
     cursor.execute('use zillow')
 
-    # create table house_unit
-    del_existing_house_unit = 'drop table if exists house_unit'
-    create_house_unit = """create table house_unit(
+    # create table unit_house
+    del_existing_unit_house = 'drop table if exists unit_house'
+    create_unit_house = """create table unit_house(
     id int primary key auto_increment,
     zillow_id int,
     price float not null, 
     zestimate float,
     house_type varchar(255),
+    image_link varchar(255),
+    unit_link varchar(255),
+    area_sq float,
+    beds int, 
+    baths int,
     UNIQUE (zillow_id)
     )ENGINE=INNODB"""
 
-    # create table unit_image
-    del_existing_unit_image = 'drop table if exists unit_image'
-    create_unit_image = """create table unit_image(
-    id int primary key,
-    image varchar(255),
-    foreign key (id) references house_unit(id)
-        on delete cascade
-        on update cascade
+    # create table air_quality_index
+    del_existing_air_quality_index = 'drop table if exists air_quality_index'
+    create_air_quality_index = """create table air_quality_index(
+    id int primary key auto_increment,
+    city varchar(255),
+    AQI int,
+    UNIQUE (city)
     )ENGINE=INNODB"""
 
-    # create table unit_address
-    del_existing_unit_address = 'drop table if exists unit_address'
-    create_unit_address = """create table unit_address(
-    id int auto_increment primary key,
+    # create table address
+    del_existing_address = 'drop table if exists address'
+    create_address = """create table address(
+    id int primary key,
     street varchar(255),
     city varchar(255),
-    state varchar(255),
     zipcode int,
-    foreign key (id) references house_unit(id)
+    foreign key (id) references unit_house(id), 
+    foreign key (city) references air_quality_index(city) 
         on delete cascade
         on update cascade
     )ENGINE=INNODB"""
 
-    # create table unit_info
-    del_existing_unit_info = 'drop table if exists unit_info'
-    create_unit_info = """create table unit_info(
-    id int auto_increment primary key,
-    beds int,
-    baths int,
-    area_sq float,
-    foreign key (id) references house_unit(id)
+    # create table schools
+    del_existing_schools = 'drop table if exists schools'
+    create_schools = """create table schools(
+      id int auto_increment primary key,
+      school_name varchar(255),
+      city varchar(255)
+      )ENGINE=INNODB"""
+
+    # create table address_to_schools
+    del_existing_address_to_schools = 'drop table if exists address_to_schools'
+    create_address_to_schools = """create table address_to_schools(
+    address_id int, 
+    school_id int,
+    foreign key (address_id) references address(id),
+    foreign key (school_id) references schools(id)
         on delete cascade
-        on update cascade 
+        on update cascade
     )ENGINE=INNODB"""
 
-    # create table unit_link
-    del_existing_unit_link = 'drop table if exists unit_link'
-    create_unit_link = """create table unit_link(
-    id int primary key,
-    link varchar(255),
-    foreign key (id) references house_unit(id)
-        on delete cascade
-        on update cascade 
-    )ENGINE=INNODB"""
+    cursor.execute(del_existing_unit_house)
+    cursor.execute(create_unit_house)
+    print('table unit_house is created.')
 
-    cursor.execute(del_existing_house_unit)
-    cursor.execute(create_house_unit)
-    print('table house_unit is created.')
+    cursor.execute(del_existing_air_quality_index)
+    cursor.execute(create_air_quality_index)
+    print('table air_quality_index is created.')
 
-    cursor.execute(del_existing_unit_image)
-    cursor.execute(create_unit_image)
-    print('table unit_image is created.')
+    cursor.execute(del_existing_address)
+    cursor.execute(create_address)
+    print('table address is created.')
 
-    cursor.execute(del_existing_unit_address)
-    cursor.execute(create_unit_address)
-    print('table unit_address is created.')
+    cursor.execute(del_existing_schools)
+    cursor.execute(create_schools)
+    print('table schools is created.')
 
-    cursor.execute(del_existing_unit_info)
-    cursor.execute(create_unit_info)
-    print('table unit_type is created.')
-
-    cursor.execute(del_existing_unit_link)
-    cursor.execute(create_unit_link)
-    print('table unit_link is created.')
+    cursor.execute(del_existing_address_to_schools)
+    cursor.execute(create_address_to_schools)
+    print('table address_to_schools is created.')
 
 
 def main():
